bdf9213e863d27f75b6a1e9dd5ca398d73da884c
Saul Canche <saulcanche.sc8@gmail.com>
2023-06-16 00:26:57 -0500

    se termino el problema 18

--- ADA2_CancheSaul/problema18.c
+++ ADA2_CancheSaul/problema18.c
@@ -3,13 +3,44 @@ Copilador: Zinjai
 Version: 1.0
 Sistema operativo: Linux ubuntu
 Fecha de cracion: 7 de junio 2023
-Descripcion:
-E.P. que lea el nombre, la antigüedad y sueldo de cada uno de los 50
-empleados y, que calcule e imprima el nombre y la cantidad que le
-corresponde a cada uno. Además, determinar e imprimir el monto total
-que debe pagar la empresa, por los incentivos otorgados. Utilizar For.*/
+Descripcion: 
+18. Se llevará a cabo en la Universidad el concurso Srita. Simpatía.
+Existen dos candidatas: Hortencia y Anacreta. E.P. que lea el voto de
+los N alumn@s y que determine e imprima el nombre de la ganadora.
+Considerar la posibilidad de votos nulos y empate en el concurso.
+Utilizar For y funciones con paso de parámetro*/
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
 int main(){
     setlocale(LC_ALL, "es_ES");
+    int i, votosAna, votosHortencia, votosNulos, numAlumns, voto; char ganadora[20];
+    /* Entradas */
+    do{
+        print("Ingrese el numero de almunos"); scanf("%i", &numAlumns);
+        if(numAlumns<0){
+            print("El numero de alumnos que ingreso es negativo \n");
+        }
+    }while(numAlumns < 0)
+    for(i = 0; i < 5; i++){
+        print("Por quien desea botar \n1) Anacreta\n2) Hortencia\nSi desea anular su voto precione cualquier otro numero entero"); scanf("%i", &voto);
+        /* Proceso*/
+        if(voto == 1){
+            votosAna ++;
+        }else if(voto == 2){
+            votosHortencia++;
+        }else{
+            votosNulos++;
+        }
+    }
+    if(votosAna > votosHortencia){
+        ganadora = "Anacreta";
+    }else if(votosAna < votosHortencia){
+        ganadora = "Hortencia"
+    }else{
+        ganadora = "empate"
+    }
+    print("hubo un total de votos de %i\nAna tuvo un total de %i\nHortencia recibio un total de%i\n El resultado fue: %s" votosAna+votosHortencia+votosNulos, votosAna, votosHortencia, ganadora);
+
+
+
