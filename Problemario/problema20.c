8e6a76a6b3fcf58b8ec9c63d980e184abe7e4825
Saul Canche <saulcanche.sc8@gmail.com>
2023-06-21 19:16:45 -0500

    se termino el problema 20 y se añadio una ruta a gitinore

--- Problemario/problema20.c
+++ Problemario/problema20.c
@@ -9,11 +9,38 @@ elemento negativo y otro positivo y, que determine e imprima su
 posición.
 */
 #include <stdio.h>
-// #include <locale.h>
+#include <locale.h>
 int main(){
-//    setlocale(LC_ALL, "es_ES");
-    printf("Hola uwu \n");
-    int num = 5;
-    printf("%i", num);
+    setlocale(LC_ALL, "es_ES");
+    int n, positivos, negativos;
+    printf("Ingrese la cantidad de numeros que deasea ingresar:  "); scanf("%i", &n);
+    int numeros[n], posPositivos[n], posNegativos[n];
+    positivos = 0; negativos = 0;
+    for(int i = 0; i < n; i++){
+        printf("Ingrese el numero %i:", i+1);
+        scanf("%d", &numeros[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        if (numeros[i] > 0)
+        {
+            posPositivos[positivos] = i;
+            positivos++;
+        }else if (numeros[i] < 0)
+        {
+            posNegativos[negativos] = i;
+            negativos++;
+        }
+    }
+    printf("Ingresaste un total de %i numeros positivos y un total de %i numeros negativos \n", positivos, negativos);
+    printf("Los numeros positivos se encuentran en las posiciones: ");
+    for (int i = 0; i < positivos; i++)
+    {
+        printf("%i, ", posPositivos[i]);
+    }
+    printf("Los numeros negativos se encuentran en las posiciones: ");
+    for (int i = 0; i < negativos; i++)
+    {
+        printf("%i, ", posNegativos[i]);
+    }
+    return 0;
 }
-
