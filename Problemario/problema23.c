dfe0a057af6e27e19176bcfbabea09c0eb12dba1
Saul Canche <saulcanche.sc8@gmail.com>
2023-06-21 22:28:50 -0500

    solucion comenzada de el problema 23 aun hay algunos problemas que resolver

--- Problemario/problema23.c
+++ Problemario/problema23.c
@@ -12,7 +12,82 @@ ser de tamaño diferenciado, dependiendo de los tipos de elementos
 encontrados en el vector original.
 */
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
+void leerVectorNumeros(int numElementos, int numeros[]);
+void imprimirVectorNumeros(int numElementos, int numeros[]);
+void vectorPostivos(int numElementos, int numeros[], int resultado[]);
+void vectorNegativos(int numElementos, int numeros[], int resultado[]);
+void vectorNulos(int numElementos, int numeros[], int resultado[]);
 int main(){
     setlocale(LC_ALL, "es_ES");
+    /* Entradas*/
+    int numElementos;
+    printf("Ingrese la cantidad de numeros que le gustaria leer: "); scanf("%i", &numElementos);
+    int numeros[numElementos];
+    int numerosPostivos[numElementos];
+    int numerosNegativos[numElementos];
+    int numerosNulos[numElementos];
+    leerVectorNumeros(numElementos, numeros);
+    
+    /*Proceso*/
+    vectorPostivos(numElementos, numeros, numerosPostivos);
+    vectorNegativos(numElementos, numeros, numerosNegativos);
+    vectorNulos(numElementos, numeros, numerosNulos);
+    
+    /*Salidas*/
+    printf("el vector de numeros negativos es el siguiente: \n");
+    imprimirVectorNumeros(numElementos, numerosNegativos);
+    printf("el vector de numeros positivos es el siguiente: \n");
+    imprimirVectorNumeros(numElementos, numerosPostivos);
+    printf("el vector de numeros nulos es el siguiente: \n");
+    imprimirVectorNumeros(numElementos, numerosNulos);
+}
+/* Proceso*/
+void leerVectorNumeros(int numElementos, int numeros[]){
+    for (int i = 0; i < numElementos; i++)
+    {
+        printf("Ingrese el numero %i :", i+1);
+        scanf("%i", &numeros[i]);
+    }
+}
+void imprimirVectorNumeros(int numElementos, int numeros[]){
+    for (int i = 0; i < numElementos; i++){
+        printf("%i, ", numeros[i]);
+    }
+}
+void vectorPostivos(int numElementos, int numeros[], int resultado[]){
+    int positivos = 0;
+    for (int i = 0; i < numElementos; i++)
+    {
+        if (numeros[i] > 0)
+        {
+            resultado[positivos] = numeros[i];
+            positivos++;
+        }
+        
+    }
+}
+void vectorNegativos(int numElementos, int numeros[], int resultado[]){
+    int negativos = 0;
+    for (int i = 0; i < numElementos; i++)
+    {
+        if (numeros[i] < 0)
+        {
+            resultado[negativos] = numeros[i];
+            negativos++;
+        }
+        
+    }
+}
+void vectorNulos(int numElementos, int numeros[], int resultado[]){
+    int nulos = 0;
+    for (int i = 0; i < numElementos; i++)
+    {
+        if (numeros[i] == 0)
+        {
+            resultado[nulos] = numeros[i];
+            nulos++;
+        }
+        
+    }
+}
