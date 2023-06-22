bb09dcdfd1aa7a92235c60d15df51567b6770543
Saul Canche <saulcanche.sc8@gmail.com>
2023-06-21 21:45:37 -0500

    solucion completa de el problema 22

--- Problemario/problema22.c
+++ Problemario/problema22.c
@@ -8,7 +8,46 @@ Descripcion:
 elemento menor y mayor del arreglo, así como su posición.
 */
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
+int leerVectorNumeros(int numElementos, int numeros[]);
+int minVector(int vector[], int numElementos);
+int maxVector(int vector[], int numElementos);
 int main(){
     setlocale(LC_ALL, "es_ES");
+    /* Entradas*/
+    int numElementos;
+    printf("Ingrese la cantidad de numeros que le gustaria leer: "); scanf("%i", &numElementos);
+    int numeros[numElementos];
+    leerVectorNumeros(numElementos, numeros);
+    /*Salidas*/
+    printf("El valor minimo ingresado es: %i y el maximo es: %i ", minVector(numeros, numElementos), maxVector(numeros, numElementos));
+}
+/* Proceso*/
+int leerVectorNumeros(int numElementos, int numeros[]){
+    for (int i = 0; i < numElementos; i++)
+    {
+        printf("Ingrese el numero %i :", i+1);
+        scanf("%i", &numeros[i]);
+    }
+    return 0;
+}
+int minVector(int vector[], int numElementos){
+    int min=vector[0];
+    for (int i = 0; i < numElementos; i++){
+        if (vector[i] < min)
+        {
+            min = vector[i];
+        }
+    }
+    return min;
+}
+int maxVector(int vector[], int numElementos){    
+    int max = vector[0];
+    for (int i = 0; i < numElementos; i++){
+        if (vector[i] > max)
+        {
+            max = vector[i];
+        }
+    }
+    return max;
+}
