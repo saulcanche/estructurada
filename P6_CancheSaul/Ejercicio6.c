b5ca0228c183aa5635477ee801172f11842728da
Saul Canche <saulcanche.sc8@gmail.com>
2023-06-22 23:03:43 -0500

    solucion del problema 6 completa

--- P6_CancheSaul/Ejercicio6.c
+++ P6_CancheSaul/Ejercicio6.c
@@ -13,6 +13,73 @@ el producto de la matriz A por el vector X, es un vector Z donde el elemento 1 e
 */
 #include <stdio.h>
 #include <locale.h>
+
+void leerMatriz(int n, int m, float matriz[n][m]) {
+    printf("Ingrese los elementos de la matriz:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%f", &matriz[i][j]);
+        }
+    }
+}
+
+void leerVectorFloat(int numElementos,float floats[], char tipo[15]) {
+    for (int i = 0; i < numElementos; i++) {
+        printf("Ingrese la/el %s %d: ",tipo,i+1);
+        scanf("%f",&floats[i]);
+    }
+}
+
+void productoMatrizVector(int n, int m, float matriz[n][m], float vectorX[n], float vectorZ[n]) {
+    for (int i = 0; i < n; i++) {
+        vectorZ[i] = 0;
+        for (int j = 0; j < m; j++) {
+            vectorZ[i] += matriz[i][j] * vectorX[j];
+        }
+    }
+}
+
+void imprimirMatriz(int n, int m, float matriz[n][m]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%.2f ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void imprimirVector(int n, float vector[n]) {
+    for (int i = 0; i < n; i++) {
+        printf("%.2f ", vector[i]);
+    }
+    printf("\n");
+}
+
 int main(){
     setlocale(LC_ALL, "es_ES"); // local español
+    int n, m;
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &n);
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &m);
+    
+    float matrizA[n][m];
+    leerMatriz(n, m, matrizA);
+    
+    float vectorX[m];
+    leerVectorFloat(m, vectorX, "elemento del vector X");
+    
+    float vectorZ[n];
+    productoMatrizVector(n, m, matrizA, vectorX, vectorZ);
+    
+    printf("Matriz A:\n");
+    imprimirMatriz(n, m, matrizA);
+    
+    printf("Vector X:\n");
+    imprimirVector(m, vectorX);
+    
+    printf("Vector Z:\n");
+    imprimirVector(n, vectorZ);
+    
+    return 0;
 }
