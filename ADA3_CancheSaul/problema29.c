2c2faceb9b7c2bf46767a22eb73c6dd45602a39c
Saul Canche <saulcanche.sc8@gmail.com>
2023-06-26 22:45:33 -0500

    se añadio la solucion dle ejercicio 29

--- ADA3_CancheSaul/problema29.c
+++ ADA3_CancheSaul/problema29.c
@@ -9,7 +9,39 @@ imprima, un vector de 5 elementos que contenga la sumatoria de cada
 fila de la matriz.
 */
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
+int m;
+void leerMatriz(int matriz[][m], int n, int m) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("Ingrese el elemento [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
+void sumatoria(int matriz[][5], int n, int m) {
+    int vector[5] = {0};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            vector[i] += matriz[i][j];
+        }
+    }
+    printf("El vector de sumatorias es: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vector[i]);
+    }
+}
 int main(){
     setlocale(LC_ALL, "es_ES");
+    int n, m;
+    printf("Ingrese el número de filas y columnas de la matriz: ");
+    scanf("%d %d", &n, &m);
+    int matriz[n][m];
+    leerMatriz(matriz, n, m);
+    sumatoria(matriz, n, m);
+    return 0;
+}
+
+
+
