fe953fda31e78506da978b87cccac94b8d86f6fa
Saul Canche <saulcanche.sc8@gmail.com>
2023-06-26 19:28:17 -0600

    se soluciono el problema 28

--- ADA3_CancheSaul/problema28.c
+++ ADA3_CancheSaul/problema28.c
@@ -8,7 +8,74 @@ Descripcion:
 imprima, cuantos elementos son pares y cuantos son impares.
 */
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
-int main(){
+#define M 3
+#define N 3
+
+void leerMatriz(int matriz[M][N])
+{
+    int i, j;
+
+    printf("Ingresa los elementos de la matriz:\n");
+
+    for(i = 0; i < M; i++)
+    {
+        for(j = 0; j < N; j++)
+        {
+            printf("Ingresa el elemento a[%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
+
+void contarParesImpares(int matriz[M][N], int *pares, int *impares)
+{
+    int i, j;
+
+    for(i = 0; i < M; i++)
+    {
+        for(j = 0; j < N; j++)
+        {
+            if(matriz[i][j] % 2 == 0)
+            {
+                (*pares)++;
+            }
+            else
+            {
+                (*impares)++;
+            }
+        }
+    }
+}
+
+void imprimirMatriz(int matriz[M][N])
+{
+    int i, j;
+
+    printf("\nLa matriz ingresada es:\n");
+
+    for(i = 0; i < M; i++)
+    {
+        for(j = 0; j < N; j++)
+        {
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
     setlocale(LC_ALL, "es_ES");
+    int matriz[M][N];
+    int pares = 0;
+    int impares = 0;
+
+    leerMatriz(matriz);
+    contarParesImpares(matriz, &pares, &impares);
+    imprimirMatriz(matriz);
+
+    printf("\nLa matriz tiene %d elementos pares y %d elementos impares.\n", pares, impares);
+
+    return 0;
+}
