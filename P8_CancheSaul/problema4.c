16f077c17a0e5a400c16f94f380146f1d8ceffc6
saulcanche <67924790+saulcanche@users.noreply.github.com>
2023-06-30 15:55:09 +0000

    se añadio solucion problema 4

--- P8_CancheSaul/problema4.c
+++ P8_CancheSaul/problema4.c
@@ -5,8 +5,54 @@ Sistema operativo: Linux ubuntu
 Fecha de cracion: 30  de junio 2023
 Descripcion:
 */
-#include <stdio.h>
+/*4. Hacer un arreglo de estructura llamada atleta para N atletas que contenga 
+los siguientes miembros: nombre, pais, numero_medallas. y devuelva los datos
+(nombre, pais) del atleta que ha ganado el mayor numero de medallas.*/
+
+#include<stdio.h>
+#include<stdlib.h>
 #include <locale.h>
+struct atleta{
+	char nombre[20];
+	char pais[20];
+	int numero_medallas;
+}atletas[100];
+
 int main(){
-    setlocale(LC_ALL, "es_ES");
+
+	setlocale(LC_ALL, "es_ES");
+	int i,n,mayor = 0,pos;
+	/* Entradas*/
+	printf("Digite el numero de atletas: ");
+	scanf("%i",&n);
+	
+	for(i=0;i<n;i++){
+		fflush(stdin);
+		printf("%i. Digite su nombre: ",i+1);
+		gets(atletas[i].nombre);
+		fflush(stdin);
+		printf("%i. Digite su pais: ",i+1);
+		gets(atletas[i].pais);
+		printf("%i. Digite el numero de medallas: ",i+1);
+		scanf("%i",&atletas[i].numero_medallas);
+		printf("\n");
+	}
+	
+    /*Proceso*/
+	for(i=0;i<n;i++){
+		if(atletas[i].numero_medallas > mayor){
+			mayor = atletas[i].numero_medallas;
+			pos = i;
+		}
+	}
+    /*Salida*/
+	
+	printf("\n--El atleta con mas medallas es: --\n");
+	printf("\nNombre: %s",atletas[pos].nombre);
+	printf("\nPais: %s\n\n",atletas[pos].pais);
+	
+	system("pause");
+	return 0;
 }
+
+  
