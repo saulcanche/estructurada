d0eb62a91893e3c19bb1ebaf8b25cb138f114044
saulcanche <67924790+saulcanche@users.noreply.github.com>
2023-06-30 15:22:43 +0000

    se corrigio la practica 8

---
+++ P8_CancheSaul/problema4.c
@@ -0,0 +1,12 @@
+/* Author: Saul Canche Marcial 
+Copilador: Zinjai
+Version: 1.0
+Sistema operativo: Linux ubuntu
+Fecha de cracion: 30  de junio 2023
+Descripcion:
+*/
+#include <stdio.h>
+#include <locale.h>
+int main(){
+    setlocale(LC_ALL, "es_ES");
+}
