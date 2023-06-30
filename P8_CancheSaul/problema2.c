c9d05f81b043aa5940a259a29490982775e8735e
saulcanche <67924790+saulcanche@users.noreply.github.com>
2023-06-30 15:46:34 +0000

    se soluciono el problema 2 y el problema 3

--- P8_CancheSaul/problema2.c
+++ P8_CancheSaul/problema2.c
@@ -5,8 +5,48 @@ Sistema operativo: Linux ubuntu
 Fecha de cracion: 30  de junio 2023
 Descripcion:
 */
+/*2. Hacer una estructura llamada alumno, en la cual se tendran los
+siguientes miembros: Nombre, edad, promedio, pedir datos al usuario 
+para 3 alumnos, comprobar cual de los 3 tiene el mejor promedio y
+posteriormente imprimir los datos del alumno*/
 #include <stdio.h>
 #include <locale.h>
+struct alumno{
+	char nombre[20];
+	int edad;
+	float promedio;
+}alumnos[3];
+
 int main(){
     setlocale(LC_ALL, "es_ES");
+    	int i,pos;
+	float mejorNota = 0.0;
+    /* Entrada*/
+	for(i=0;i<3;i++){
+		fflush(stdin);
+		printf("%i. Digite su nombre: ",i+1);
+		gets(alumnos[i].nombre);
+		printf("%i. Digite su edad: ",i+1);
+		scanf("%i",&alumnos[i].edad);
+		printf("Digite su promedio: ",i+1);
+		scanf("%f",&alumnos[i].promedio);
+		printf("\n");
+	}
+	
+    /*Proceso*/
+	for(i=0;i<3;i++){
+		if(alumnos[i].promedio > mejorNota){
+			mejorNota = alumnos[i].promedio;
+			pos = i;
+		}
+	}
+    /* SAlida*/
+	//Imprimimos los datos del mejor promedio
+	printf("\nEl alumno con la mejor nota es: \n");
+	printf("\nNombre: %s",alumnos[pos].nombre);
+	printf("\nEdad: %i",alumnos[pos].edad);
+	printf("\nPromedio: %.2f\n\n",alumnos[pos].promedio);
+	
+	system("pause");
+	return 0;
 }
--- P8_CancheSaul/problema3.c
+++ P8_CancheSaul/problema3.c
@@ -5,8 +5,61 @@ Sistema operativo: Linux ubuntu
 Fecha de cracion: 30  de junio 2023
 Descripcion:
 */
-#include <stdio.h>
+/*3. Realizar un programa en C que lea un arreglo de estructuras los datos
+de N empleados de la empresa y que imprima los datos del empleado con
+mayor y menor salario.*/
+
+#include<stdio.h>
+#include<stdlib.h>
 #include <locale.h>
+struct empleado{
+	char nombre[20];
+	char sexo[20];
+	float salario;	
+}empleados[100];
+
 int main(){
     setlocale(LC_ALL, "es_ES");
+	int i,n,pmay,pmen;
+	float mayor = 0.0, menor = 9999999;
+	printf("Digite el numero de empleados de la empresa: ");
+	scanf("%i",&n);
+    /*Entradas*/
+	for(i=0;i<n;i++){
+		fflush(stdin);
+		printf("%i. Digite su nombre: ",i+1);
+		gets(empleados[i].nombre);
+		fflush(stdin);
+		printf("%i. Digite su sexo: ",i+1);
+		gets(empleados[i].sexo);
+		printf("%i. Digite su salario: ",i+1);
+		scanf("%f",&empleados[i].salario);
+		printf("\n");
+	}
+    /* Proceso*/
+	//Comprobando el mayor y menor Salario
+	for(i=0;i<n;i++){
+		if(empleados[i].salario > mayor){ 
+			mayor = empleados[i].salario;
+			pmay = i;
+		}
+		else if(empleados[i].salario < menor){
+			menor = empleados[i].salario;
+			pmen = i;
+		}
+	}
+	
+    /* Salidas*/
+	printf("\nEl empleado con el mayor salario es: \n");
+	printf("\nNombre: %s",empleados[pmay].nombre);
+	printf("\nSexo: %s",empleados[pmay].sexo);
+	printf("\nSalario: %.2f",empleados[pmay].salario);
+	
+	printf("\n\nEl empleado con el menor salario es: \n");
+	printf("\nNombre: %s",empleados[pmen].nombre);
+	printf("\nSexo: %s",empleados[pmen].sexo);
+	printf("\nSalario: %.2f\n",empleados[pmen].salario);
+	
+	system("pause");
+	return 0;
 }
