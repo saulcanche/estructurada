04eb07f62e7ce6cb25dbda1e110803f844ebdaa1
Saul Canche <saulcanche.sc8@gmail.com>
2023-07-03 20:08:15 -0500

    sol 39

--- ADA4_CancheSaul/problema39.c
+++ ADA4_CancheSaul/problema39.c
@@ -11,7 +11,53 @@ elementos e imprimir, la dirección de memoria y los elementos del
 arreglo resultante.
 */
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
-int main(){
+#include <stdlib.h>
+int main() {
     setlocale(LC_ALL, "es_ES");
+    long *arreglo = (long*)malloc(5 * sizeof(long));
+
+    if (arreglo == NULL) {
+        printf("Error: No se pudo asignar memoria.\n");
+        return 1;
+    }
+
+    printf("Dirección de memoria del arreglo original: %p\n", (void*)arreglo);
+
+    printf("Ingrese los 5 números enteros largos:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%ld", &arreglo[i]);
+    }
+
+    printf("Elementos del arreglo original:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%ld ", arreglo[i]);
+    }
+    printf("\n");
+
+    arreglo = (long*)realloc(arreglo, 10 * sizeof(long));
+
+    if (arreglo == NULL) {
+        printf("Error: No se pudo ampliar el arreglo.\n");
+        return 1;
+    }
+
+    printf("Dirección de memoria del arreglo ampliado: %p\n", (void*)arreglo);
+
+    printf("Ingrese los 5 números enteros largos adicionales:\n");
+    for (int i = 5; i < 10; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%ld", &arreglo[i]);
+    }
+
+    printf("Elementos del arreglo ampliado:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%ld ", arreglo[i]);
+    }
+    printf("\n");
+
+    free(arreglo);
+
+    return 0;
+}
