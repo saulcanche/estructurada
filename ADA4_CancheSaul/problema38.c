e239f1d4addfda9e08c37118a5b7867d44a60323
Saul Canche <saulcanche.sc8@gmail.com>
2023-07-03 20:04:31 -0500

    sol 38

--- ADA4_CancheSaul/problema38.c
+++ ADA4_CancheSaul/problema38.c
@@ -11,7 +11,32 @@ leer y luego, imprimir la dirección de memoria y los elementos de dicho
 arreglo; si no, indicar un error en la asignación de memoria.
 */
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
+#include <stdlib.h>
 int main(){
     setlocale(LC_ALL, "es_ES");
+    double *arreglo = (double*)malloc(10 * sizeof(double));
+
+    if (arreglo == NULL) {
+        printf("Error: No se pudo asignar memoria.\n");
+        return 1;
+    }
+
+    printf("Dirección de memoria del arreglo: %p\n", (void*)arreglo);
+
+    printf("Ingrese los 10 números reales largos:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%lf", &arreglo[i]);
+    }
+
+    printf("Elementos del arreglo:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%lf ", arreglo[i]);
+    }
+    printf("\n");
+
+    free(arreglo);
+
+    return 0;
+    }
